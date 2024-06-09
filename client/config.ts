0eef941c5aec5997c2f3158361005e2e94673c8d
Ashish Pal <aashishpal50@gmail.com>
2024-06-09 08:32:10 +0530

    Config url fixed in client

--- client/config.ts
+++ client/config.ts
@@ -6,5 +6,5 @@ export const config = {
   RETRY_EXPONENTIAL_MULTIPLIER: 500,
   MAX_REQUEST_RETRY: 3,
   SCRAPER_API_URL: process.env.SCRAPER_API_URL,
-  USE_CSE_PAPI: process.env.USE_CSE_PAPI,
+  USE_CSE_PAPI: !!process.env.USE_CSE_PAPI || true,
 };
